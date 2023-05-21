b9dc2ea137d1405bb27e38027a768310b96b74ba
HilquiasAbias <hilquias.dev@gmail.com>
2023-05-21 17:38:35 -0300

    iniciando alteração para typescript

---
+++ typescript-feature/src/errors/main.ts
@@ -0,0 +1,10 @@
+/* eslint-disable import/extensions */
+import { ErrorType } from './types';
+
+// {...}
+
+export default (exception: ErrorType) => {
+  // freeze system
+
+  console.log(exception.error);
+};
---
+++ typescript-feature/src/index.ts
@@ -0,0 +1,8 @@
+/* eslint-disable import/extensions */
+
+import errors from './errors/main';
+
+errors({
+  module: 'back',
+  error: 'back_without_instructions',
+});
